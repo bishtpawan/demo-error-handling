b590ba391135b07948276b10e9f6694ff5a33230
bishtpawan <pawan.bisht@knoldus.in>
2019-02-24 20:49:58 +0530

    Add recoverable error with match expression

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-///This program is intended to show the unrecoverable error.
-fn main() {
-    let count: vec<i32> = vec![1,2,3];
-    print!("element of a vector is {}:",count[3]);
+use std::fs::File;
+use std::io::Error;
+
+
+///This program is intended to show the recoverable error with match keyword.
+
+fn main()
+{
+    let data: Result<File,Error> = File::open("new_file.txt");
+    match data
+        {
+            Ok(file) => file,
+            Err(error) => {
+                panic!("Unable to open file: {:?}", error)
+            },
+        };
 }
+
