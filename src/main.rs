685b10ea72c3a0e1180378a1c57a01dc6a23615b
bishtpawan <pawan.bisht@knoldus.in>
2019-02-24 21:16:56 +0530

    Implement propagation with shortcut

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,17 @@ use demo::utilities::constant::FILE_PATH;
 
 
 
-///This program is intended to show the recoverable error with match expression shortcuts(unwrap() and expect()).
+///This program is intended to show the recoverable error with shortcut of propagating errors.
 
 fn main()
 {
-    let data: Result<String, Error> = read_content_from_file();
-    println!("{:?}",data);
+    println!("{:?}",read_content_from_file());
 }
 fn read_content_from_file() -> Result<String, Error>
 {
-    let mut read_file: File = match  File::open(FILE_PATH) {
-        Ok(file) => file,
-        Err(error) => return Err(error),
-    };
+    let mut read_file: File =File::open(FILE_PATH)?;
     let mut content: String = String::new();
-    match read_file.read_to_string(&mut content) {
-        Ok(_) => Ok(content),
-        Err(error) => Err(error),
-    }
+    read_file.read_to_string(&mut content)?;
+    Ok(content)
 }
 
