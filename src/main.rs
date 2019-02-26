d6eead81dde353847cf944da2e95fc1202a318c4
bishtpawan <pawan.bisht@knoldus.in>
2019-02-26 21:20:06 +0530

    Add test cases

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,21 @@ fn main()
 
     //File::open(INVALID_FILE_PATH).expect("Unable to open file");
 }
+#[cfg(test)]
+mod test {
+    use std::fs::File;
+    use demo::utilities::constant::INVALID_FILE_PATH;
 
+    #[test]
+    #[should_panic]
+    fn test_shortcut_unwrap(){
+        File::open(INVALID_FILE_PATH).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_shortcut_expect(){
+        File::open(INVALID_FILE_PATH).expect("Unable to open file");
+    }
+
+}
