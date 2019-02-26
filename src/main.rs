98d3c4f84553d05d31940ae8cca06e7b0f9190db
bishtpawan <pawan.bisht@knoldus.in>
2019-02-26 22:13:35 +0530

    Add test case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 ///This program is intended to show the unrecoverable error.
 fn main() {
-    let count: vec<i32> = vec![1,2,3];
+    display_unrecoverable_error();
+}
+fn display_unrecoverable_error(){
+    let count: Vec<i32> = vec![1,2,3];
     print!("element of a vector is {}:",count[3]);
+}
+
+
+#[cfg(test)]
+mod test {
+    use crate::display_unrecoverable_error;
+
+    # [test]
+    # [should_panic]
+    fn test_display_unrecoverable_error(){
+        display_unrecoverable_error();
+    }
 }
